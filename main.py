dbb5eb4d4c6fd271a79ef6432e1ffc8c31d5a010
SkumarGithub12 <143387077+SkumarGithub12@users.noreply.github.com>
2024-01-18 18:26:41 +0000

    chnages

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
-# print("Day 1 - Python Print Function")
+# # print("Day 1 - Python Print Function")
 
-# print("Day 1 - String Manipulation")
-# print('String Concatenation is done with the "+" sign.')
-# print('e.g. print("Hello " + "world")')
-# print("New lines can be created with a backslash and n.")
+# # print("Day 1 - String Manipulation")
+# # print('String Concatenation is done with the "+" sign.')
+# # print('e.g. print("Hello " + "world")')
+# # print("New lines can be created with a backslash and n.")
 
-# print("Hello World!")
-# print("Hello World!\nHello World!\nHello World!")
-# print("hello" +" "+ "friend")
-# print(len(input("what is your name?")))
+# # print("Hello World!")
+# # print("Hello World!\nHello World!\nHello World!")
+# # print("hello" +" "+ "friend")
+# # print(len(input("what is your name?")))
 
-# name=input()
-# print(len(name))
+# # name=input()
+# # print(len(name))
 
-# name = input("what is your name?")
-# len = len(name)
-# print(len)
+# # name = input("what is your name?")
+# # len = len(name)
+# # print(len)
 
-# name = "Orange"
-# print(name)
+# # name = "Orange"
+# # print(name)
 
-# name = "Apple"
-# print(name)
+# # name = "Apple"
+# # print(name)
 
-# Myname = "god"
-# Age = 100
-# print(Myname)
-# print(Age)
+# # Myname = "god"
+# # Age = 100
+# # print(Myname)
+# # print(Age)
 
-# There are two variables, a and b
-a = input()
-b = input()
-# Create a third variable to help switch the values
-c = a
-a = b
-b = c
-print("a: " + a)
-print("b: " + b)
+# # There are two variables, a and b
+# a = input()
+# b = input()
+# # Create a third variable to help switch the values
+# c = a
+# a = b
+# b = c
+# print("a: " + a)
+# print("b: " + b)
