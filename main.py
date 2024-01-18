1e01a43da466264d3685c56d7b1e3dec1cede292
SkumarGithub12 <143387077+SkumarGithub12@users.noreply.github.com>
2024-01-18 18:25:18 +0000

    chnage

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+# print("Day 1 - Python Print Function")
+
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
 # print("Hello World!")
 # print("Hello World!\nHello World!\nHello World!")
 # print("hello" +" "+ "friend")
@@ -15,3 +22,18 @@
 
 # name = "Apple"
 # print(name)
+
+# Myname = "god"
+# Age = 100
+# print(Myname)
+# print(Age)
+
+# There are two variables, a and b
+a = input()
+b = input()
+# Create a third variable to help switch the values
+c = a
+a = b
+b = c
+print("a: " + a)
+print("b: " + b)
