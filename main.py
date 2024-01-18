d5c901ca40d5136d347676efbb244e34f106eb6b
SkumarGithub12 <143387077+SkumarGithub12@users.noreply.github.com>
2024-01-18 17:29:51 +0000

    Changes

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-print("hello!\nhello!")
+# print("Hello World!")
+# print("Hello World!\nHello World!\nHello World!")
+# print("hello" +" "+ "friend")
+# print(len(input("what is your name?")))
 
-print("hello"+"world")
-
-print("hello"+" "+"world")
-
-print("Day 1 - String Manipulation")
-print('String Concatenation is done with the "+" sign.')
-print('e.g. print("Hello " + "world")')
-print("New lines can be created with a backslash and n.")
+# name=input()
+# print(len(name))
